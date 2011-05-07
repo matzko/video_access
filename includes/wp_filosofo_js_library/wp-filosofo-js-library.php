9754b1fc66344e0dd53908d6db38ec16fa0ac87a
Austin Matzko <austin@pressedcode.com>
2011-05-06 22:33:03 -0500

    subtree-merge wp_filosofo_js_library

---
+++ includes/wp_filosofo_js_library/wp-filosofo-js-library.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Filosofo JavaScript Library
+Plugin URI: http://austinmatzko.com/wordpress-plugins/wp-filosofo-js-library/
+Description: A library of common JS functionality used by Austin Matzko (filosofo) in WordPress plugins.
+Author: Austin Matzko
+Author URI: http://austinmatzko.com
+Version: 1.0
+*/
+
+if ( ! function_exists( 'init_filosofo_js_common_library' ) ) {
+
+	function init_filosofo_js_common_library()
+	{
+		wp_register_script( 
+			'filosofo-common-js',
+			plugin_dir_url( __FILE__ ) . 'client-files/js/filosofo-js.js',
+			null,
+			'1.0'
+		);
+	}
+	
+	function enqueue_filosofo_js_common_library()
+	{
+		wp_enqueue_script( 'filosofo-common-js' );
+	}
+
+	add_action( 'init', 'init_filosofo_js_common_library' );
+	add_action( 'wp_enqueue_scripts', 'enqueue_filosofo_js_common_library' );
+}
+
+// eof
